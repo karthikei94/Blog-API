f5c4d44ef28309139b350ed626cb46be12d05c53
karthikeya-lti <mangalpally.10705417@ltimindtree.com>
2024-12-02 08:02:52 +0530

    Adding the Initial Code base with Sln and FirebaseAuthService

---
+++ BlogApp/Services/FirebaseAuthService.cs
@@ -0,0 +1,24 @@
+﻿using Firebase.Auth;
+
+namespace BlogApp
+{
+
+    public class FirebaseAuthService(FirebaseAuthClient firebaseAuthClient) : IFirebaseAuthService
+    {
+        private readonly IFirebaseAuthClient firebaseAuthClient = firebaseAuthClient;
+
+        public async Task<string?> SignUp(string email, string password)
+        {
+            var userCred = await firebaseAuthClient.CreateUserWithEmailAndPasswordAsync(email, password);
+            return userCred is null ? null : await userCred.User.GetIdTokenAsync();
+        }
+
+        public async Task<string?> Login(string email, string password)
+        {
+            var userCred = await firebaseAuthClient.SignInWithEmailAndPasswordAsync(email, password);
+            return userCred is null ? null : await userCred.User.GetIdTokenAsync();
+        }
+
+        public void SignOut() => firebaseAuthClient.SignOut();
+    }
+}
---
+++ BlogApp/Services/IFirebaseAuthService.cs
@@ -0,0 +1,9 @@
+﻿namespace BlogApp
+{
+    public interface IFirebaseAuthService
+    {
+        Task<string?> Login(string email, string password);
+        void SignOut();
+        Task<string?> SignUp(string email, string password);
+    }
+}
