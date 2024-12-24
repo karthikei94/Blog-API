50e4a73ce4ab2e87952febb922a0cd507223ec76
Karthikeya <mangalpally.10705417@ltimindtree.com>
2024-12-24 23:01:48 +0530

    Minor changes

--- BlogApp/Models/BlogPost.cs
+++ BlogApp/Models/BlogPost.cs
@@ -7,13 +7,13 @@ public class BlogPost
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
     public string? Id { get; set; }
-    public required string Title { get; set; } = null!;
+    public string Title { get; set; } = null!;
     public List<Content> Contents { get; set; } = [];
     public List<string> Tags { get; set; } = [];
-    public DateTime? PublicationDate { get; set; }
+    public DateTime PublicationDate { get; set; }
     public bool Draft { get; set; }
     public DateTime CreatedDate { get; set; }
-    public required string CreatedBy { get; set; } = null!;
+    public string CreatedBy { get; set; } = null!;
     public int Likes { get; set; }
     public int Views { get; set; }
 }
